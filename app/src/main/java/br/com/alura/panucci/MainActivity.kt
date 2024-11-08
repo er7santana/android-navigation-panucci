3ae144f5a8857c3e085d042d3082c21eaa89926b
Eliezer Sant Ana <er.7santana@gmail.com>
2024-11-08 10:03:48 -0300

    instalanddo lib Navigation

--- app/src/main/java/br/com/alura/panucci/MainActivity.kt
+++ app/src/main/java/br/com/alura/panucci/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import br.com.alura.panucci.sampledata.bottomAppBarItems
 import br.com.alura.panucci.sampledata.sampleProductWithImage
 import br.com.alura.panucci.sampledata.sampleProducts
@@ -27,6 +28,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            Log.i("MainActivity", "onCreate: navController $navController")
             val initialScreen = "Destaques"
             val screens = remember {
                 mutableStateListOf(initialScreen)
