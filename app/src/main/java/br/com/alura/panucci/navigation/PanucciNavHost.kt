0b8cd636749947d45d06fce860eb041f593df074
Eliezer Sant Ana <er.7santana@gmail.com>
2024-11-08 22:20:34 -0300

    removendo AppDestination class

--- app/src/main/java/br/com/alura/panucci/navigation/PanucciNavHost.kt
+++ app/src/main/java/br/com/alura/panucci/navigation/PanucciNavHost.kt
@@ -1,9 +1,14 @@
 package br.com.alura.panucci.navigation
 
 import android.content.Context
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material.icons.filled.RestaurantMenu
+import androidx.compose.material.icons.outlined.LocalBar
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import br.com.alura.panucci.ui.components.BottomAppBarItem
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
@@ -20,7 +25,23 @@ fun PanucciNavHost(navController: NavHostController, scope: CoroutineScope, cont
     }
 }
 
-
+val bottomAppBarItems = listOf(
+    BottomAppBarItem(
+        label = "Destaques",
+        icon = Icons.Filled.AutoAwesome,
+        destination = highlightsListRoute
+    ),
+    BottomAppBarItem(
+        label = "Menu",
+        icon = Icons.Filled.RestaurantMenu,
+        destination = menuRoute
+    ),
+    BottomAppBarItem(
+        label = "Bebidas",
+        icon = Icons.Outlined.LocalBar,
+        destination = drinksRoute
+    ),
+)
 
 
 
